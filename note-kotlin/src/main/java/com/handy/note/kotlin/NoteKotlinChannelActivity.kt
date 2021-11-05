dffaebf0257dffca85aac9208f7296ab2f939744
handyzhou <byhook@163.com>
2021-11-05 13:11:23 +0800

    第一个kotlin的Channel的send和receive示例

--- note-kotlin/src/main/java/com/handy/note/kotlin/NoteKotlinChannelActivity.kt
+++ note-kotlin/src/main/java/com/handy/note/kotlin/NoteKotlinChannelActivity.kt
@@ -7,19 +7,21 @@ import android.util.Log
 import android.view.View
 import com.handy.note.base.BaseNoteActivity
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 
 /**
  * date: 2021-10-27
  * description:
  * 异步流
- * https://book.kotlincn.net/text/flow.html
+ * https://book.kotlincn.net/text/channels.html
+ * 一个 Channel 是一个和 BlockingQueue 非常相似的概念。其中一个不同是它代替了阻塞的 put 操作并提供了挂起的 send，还替代了阻塞的 take 操作并提供了挂起的 receive。
  */
 class NoteKotlinChannelActivity : BaseNoteActivity() {
 
     companion object {
 
-        const val TAG = "NoteKotlinFlowActivity"
+        const val TAG = "NoteKotlinChannelPage"
 
         @JvmStatic
         fun intentStart(context: Context) {
@@ -32,6 +34,19 @@ class NoteKotlinChannelActivity : BaseNoteActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_kotlin_channel_layer)
+    }
+
+    fun onSendReceiveClick(view: View?) {
+        val channel = Channel<Int>()
+        GlobalScope.launch(Dispatchers.Default) {
+            //这里可能是消耗大量 CPU 运算的异步逻辑，我们将仅仅做5次整数的平方并发送
+            Log.d(TAG, "[${Thread.currentThread().name}] send start")
+            for (x in 1..5) channel.send(x * x)
+        }
+        //这里我们打印了5次被接收的整数：
+        GlobalScope.launch(Dispatchers.Main) {
+            repeat(5) { Log.d(TAG, "[${Thread.currentThread().name}] receive:${channel.receive()}") }
+        }
     }
 
 }
